11962af5ed49ac09d2a9dd38ca33011a13abeb84
Abdikarim1999 <abdikarim4581@gmail.com>
2022-09-13 12:18:41 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+
+char myvar = abdi;
+
+while (myvar)
+{
+
+	_putchar (myvar);
+
+	myvar++
+
+}
+
+_putchar ("\n");
+
+
+return (0);
+
+
+
+}
